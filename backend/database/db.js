608dc1d79bfe24e63dd2dd11f08aac9b1f04b163
Javier Gonzalez <javgon2306@gmail.com>
2024-10-31 15:18:39 -0300

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const url = 'mongodb://localhost:27017/mern';
+mongoose.connect(url);
+
+const db = mongoose.connection;
+db.on('open', () => {"Connected to MongoDB"})
+db.on('error', (err) => {"Error connecting to MongoDB"})
+
+export default db;
