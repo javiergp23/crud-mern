af4641ce2bbd8c924315425ce48641152e472d76
Javier Gonzalez <javgon2306@gmail.com>
2024-10-31 16:28:31 -0300

    create frontend-react

---
+++ frontend/crud-mern-frontend/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './src/App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createRoot(document.querySelector('#app')).render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+)
